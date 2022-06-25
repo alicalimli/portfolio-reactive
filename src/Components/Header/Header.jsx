2497eac379023de4e259df71758f4b24c5e7f6c0
alicalimli <alicalimli76@gmail.com>
2022-06-25 09:06:42 +0300

    added name in the header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,13 @@
 const Header = () => {
   return (
-    <div className="h-screen w-screen bg-gray-900">
+    <div className="h-screen w-screen bg-gray-900 flex items-center p-4">
       <div className="flex flex-row md:flex-col">
         <div className="text-left">
-          <h2 className="text-white text-3xl font-bold">Hello, I'm</h2>
+          <h2 className="text-white text-4xl font-bold">Hello, I'm</h2>
+          <h1 className="text-white text-6xl font-bold">Ali Calimli</h1>
+          <p className="text-slate-200">
+            A Front-end web develope based from turkey
+          </p>
         </div>
       </div>
     </div>
