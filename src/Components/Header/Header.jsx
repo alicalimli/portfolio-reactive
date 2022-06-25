a3b3fac59e3639b57abd5da042b22236140bccaa
alicalimli <alicalimli76@gmail.com>
2022-06-25 16:22:10 +0300

    layoutt adjustments in header section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,16 @@
+import profileImg from "../../Assets/my-profile.png";
+
 const Header = () => {
   return (
-    <div className="h-screen w-screen bg-gray-900 flex items-center p-4">
-      <div className="flex flex-row md:flex-col">
-        <div className="text-left">
-          <h2 className="text-white text-4xl font-bold">Hello, I'm</h2>
-          <h1 className="text-white text-6xl font-bold">Ali Calimli</h1>
-          <p className="text-slate-200">
-            A Front-end web develope based from turkey
-          </p>
-        </div>
+    <div className="h-screen w-screen bg-gray-900 flex flex-col lg:flex-row  gap-3  items-center justify-center lg:justify-between p-4 lg:px-44">
+      <div className="text-left min-w-max">
+        <h2 className="text-white text-5xl font-bold">Hello, I am</h2>
+        <h1 className="text-white text-7xl font-bold">Ali Calimli</h1>
+        <p className="text-slate-200 text-lg">
+          A Front-end web developer based from turkey
+        </p>
       </div>
+      <img src={profileImg} className="w-84" alt="" />
     </div>
   );
 };
