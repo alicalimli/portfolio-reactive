22d8a22e5057eaa1ade20b2929ad65e40d709e0c
alicalimli <alicalimli76@gmail.com>
2022-06-25 18:02:10 +0300

    created seperate project component

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,5 @@
-import { AiOutlineLink } from "react-icons/ai";
-
 import { projectsObj } from "../../projectsDB";
+import Project from "../Project/Project.jsx";
 
 const Projects = () => {
   return (
@@ -9,21 +8,8 @@ const Projects = () => {
         My Projects
       </h2>
       <div className="flex flex-wrap gap-4 justify-center lg:justify-start">
-        {projectsObj.map((project, i) => (
-          <a
-            href={project.link}
-            className="inline-block group relative rounded w-80 h-80 bg-contain bg-no-repeat"
-            key={project.title + i}
-            style={{
-              backgroundImage: `url(${project.preview})`,
-            }}
-          >
-            <div className="absolute top-0 left-0 w-full h-full bg-cyan-500/20"></div>
-            <AiOutlineLink
-              className="absolute top-1/2 left-1/2 -translate-x-1/2 
-                      -translate-y-1/2 h-16 w-16 text-cyan-500 scale-150 opacity-0 duration-300 group-hover:opacity-100 group-hover:scale-100"
-            />
-          </a>
+        {projectsObj.map((project) => (
+          <Project project={project} key={project.title} />
         ))}
       </div>
     </div>
