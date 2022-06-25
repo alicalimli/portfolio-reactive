6e827ba297f86bb9e4e3288ae7fefba5d5cfeadc
alicalimli <alicalimli76@gmail.com>
2022-06-25 17:58:25 +0300

    added more projects in data object

--- src/projectsDB.js
+++ src/projectsDB.js
@@ -1,10 +1,12 @@
+import movieMatrixPreview from "./Assets/movie-matrix-preview.jpg";
 import clipBoardPreview from "./Assets/clipboard-preview.jpg";
+import todoListPreview from "./Assets/todolist-preview.jpg";
 
 export const projectsObj = [
   {
-    title: "Clipboard",
-    preview: clipBoardPreview,
-    link: "https://clipboard-ali.netlify.app/",
+    title: "Movie Matirx",
+    preview: movieMatrixPreview,
+    link: "https://moviematrix.netlify.app/",
   },
   {
     title: "Clipboard",
@@ -12,8 +14,8 @@ export const projectsObj = [
     link: "https://clipboard-ali.netlify.app/",
   },
   {
-    title: "Clipboard",
-    preview: clipBoardPreview,
-    link: "https://clipboard-ali.netlify.app/",
+    title: "Todo List",
+    preview: todoListPreview,
+    link: "https://todo-list-ali.netlify.app/",
   },
 ];
