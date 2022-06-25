9b7f9f9f37208c22a774dc2c6e9e30073dc429b9
alicalimli <alicalimli76@gmail.com>
2022-06-25 17:07:48 +0300

    added website image preview

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,7 +1,15 @@
+import clipBoardPreview from "../../Assets/clipboard-preview.jpg";
+
 const Projects = () => {
   return (
-    <div>
-      <h2 className="text-5xl text-white">My Projects</h2>
+    <div className="flex flex-col gap-12 p-4 lg:ml-52 ">
+      <h2 className="text-4xl font-bold text-white">My Projects</h2>
+      <div className="flex flex-wrap">
+        <div className="relative rounded ">
+          <div className="absolute top-0 left-0 w-full h-full bg-cyan-500/20"></div>
+          <img src={clipBoardPreview} alt="" className="rounded" />
+        </div>
+      </div>
     </div>
   );
 };
