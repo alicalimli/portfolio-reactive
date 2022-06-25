0eee454f2df6c3b5e12a424c385652f66ba8345d
alicalimli <alicalimli76@gmail.com>
2022-06-25 16:27:47 +0300

    added contact me button

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,10 +5,15 @@ const Header = () => {
     <div className="h-screen w-screen bg-gray-900 flex flex-col lg:flex-row  gap-3  items-center justify-center lg:justify-between p-4 lg:px-44">
       <div className="text-left min-w-max">
         <h2 className="text-white text-5xl font-bold">Hello, I am</h2>
-        <h1 className="text-white text-7xl font-bold">Ali Calimli</h1>
+        <h1 className="text-white text-7xl font-bold">
+          <span className="text-cyan-500">Ali</span> Calimli
+        </h1>
         <p className="text-slate-200 text-lg">
           A Front-end web developer based from turkey
         </p>
+        <button className="border border-cyan-500 rounded duration-300 hover:bg-cyan-500 p-2 px-4 mt-5 text-white">
+          Contact me
+        </button>
       </div>
       <img src={profileImg} className="w-84" alt="" />
     </div>
